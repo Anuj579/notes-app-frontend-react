380d64d581f9280e5c7ebd6931f8b8a2347d1249
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-11-03 18:00:07 +0530

    theme context added

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext()
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState(() => {
+        return localStorage.getItem('theme') || 'light';
+    });
+
+    useEffect(() => {
+        document.body.classList.remove('light', 'dark');
+        document.body.classList.add(theme);
+        localStorage.setItem('theme', theme)
+    }, [theme])
+
+    const lightTheme = () => {
+        setTheme('light')
+    }
+    const darkTheme = () => {
+        setTheme('dark')
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, lightTheme, darkTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
+
+export const useTheme = () => useContext(ThemeContext);
